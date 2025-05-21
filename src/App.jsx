85ee2f52381bf2816bd6f28d6c12446592f53af1
Erfan Safarzad <erfansafarzad7@gmail.com>
2025-05-21 12:40:19 +0330

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./sections/header";
-
+import Hero from "./sections/hero";
 
 export default function App() {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
   )
 }
