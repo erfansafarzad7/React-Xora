d5018addbef10f0c3398b12eedcb9ccba2affba0
Erfan Safarzad <erfansafarzad7@gmail.com>
2025-05-21 17:42:21 +0330

    features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Header from "./sections/header";
-import Hero from "./sections/hero";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+
 
 export default function App() {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
+      <Features />
     </main>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import {Marker} from "./marker";
+import {Marker} from "./Marker";
 
 
 const Button = ({ icon, children, href, containerClassName, onClick, markerFill }) => {
